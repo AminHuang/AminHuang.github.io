ea0443991c8f3bc8022e03bc8a63591e8695d798
AminHuang <skuare520@gmail.com>
2015-10-24 22:17:22 +0800

    restart

---
+++ js/global.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+  getInfo("info.json");
+
+});
+
+function getInfo(src) {
+    $.getJSON(src,function(data){
+
+      
+      for (var i = 0; i < data.length; i++) {
+        var name = data[i].name;
+        var url = data[i].url;
+        var details = data[i].details;
+
+        var tmpdetails = "";
+        for(var i = 0; i < details.length; i++) {
+          console.log(details[i]);
+          tmpdetails += "<p>" + details[i].detail + "</p>";
+        }
+
+        var html = "<div class=' col-xs-12 col-sm-4 col-md-3 col-lg-3 wrap'>" +
+                   "<div class='wrap-title'> " +
+                   "<h2><a href='" + url + "' target='_blank'>" + name + "</a></h2>" +
+                   "</div>" +
+                   "<div class='detail'>" + tmpdetails +
+                   "<a href='" + url + "' target='_blank' class='btn btn-info wrap-btn'>了解更多</a>" +
+                   "</div>" +
+                   "</div>";
+
+        $("#website").append(html);
+        }
+    });
+}
